f532fb9f31f5064fbd172f9ab5b0bddfe60569ff
Oleksandr Kalienchenko <owheels.qa@gmail.com>
2023-06-07 10:56:31 +0300

    Task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+function timeUpdate(data) {
+  let currentTime = data.seconds;
+  console.log(currentTime);
+  localStorage.setItem('storage-currentTime', currentTime);
+}
+
+const storedTime = localStorage.getItem('storage-currentTime');
+console.log(storedTime);
+
+player.setCurrentTime(storedTime);
